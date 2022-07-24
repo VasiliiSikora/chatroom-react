9fa7152170adf9e7f9dd4a9ccdcbf6bcd0f80f92
Will Sikora <vasiliisikora@gmail.com>
2022-07-24 10:46:22 +1000

    setting up app

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require('dotenv').config
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Logging Middleware NEEDS TO BE BEFORE THE ROUTES (i.e. here)
+app.use((req, res, next) => {
+    console.log(`${new Date()} ${req.method} ${req.path}`);
+    next()
+})
+
+app.use(express.json());
+app.use(express.static('./client/build'))
+
+app.get("api/test", (req,res) => {
+    res.json({ result: "success" })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
